fc4d3307b5b698e4f6f9d5e868f7940316f2e142
ali (zeus) <131793522+iPmartNetwork@users.noreply.github.com>
2025-05-09 23:31:55 +0330

    iPWG

---
+++ src/static/app/src/components/settingsComponent/dashboardTheme.vue
@@ -0,0 +1,14 @@
+
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    document.documentElement.setAttribute('data-bs-theme', 'dark');
+    document.body.style.backgroundColor = '#0D1B2A';
+    document.body.style.color = '#FFA500';
+  }
+}
+</script>
